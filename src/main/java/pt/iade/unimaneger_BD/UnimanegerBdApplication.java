cf632afb0cb80c0b33f04472177db71e26c094c9
João Belo <jrmhbelo@gmail.com>
2021-01-27 12:44:37 +0000

    1stcommitTUT6

---
+++ src/main/java/pt/iade/unimaneger_BD/UnimanegerBdApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.unimaneger_BD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimanegerBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimanegerBdApplication.class, args);
+	}
+
+}
